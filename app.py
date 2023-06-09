2704a530173fe74c8337031ec2dc64290a5bd6c2
Anthony Lukach <anthonylukach@gmail.com>
2023-06-09 13:28:23 -0700

    Add prototype of building assets outside of CDK
    (untested)

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from aws_cdk import aws_lambda
 from aws_cdk.core import App, Stack 
 
@@ -6,13 +8,20 @@ class LambdaStack(Stack):
         super().__init__(app, construct_id, **kwargs)
         self.construct_id = construct_id
         
-        # create the lambda with code from a Dockerfile in ./lambda/Dockerfile
-        code_from_docker = aws_lambda.Code.from_docker_build(path='./lambda', platform='linux/amd64')
+        code = (
+            # create lambda with assets found in specified directory
+            aws_lambda.Code.from_asset(ASSET_DIR)
+            if (ASSET_DIR := os.getenv("BUILT_ASSET"))
+            # create the lambda with code from a Dockerfile in ./lambda/Dockerfile
+            else aws_lambda.Code.from_docker_build(
+                path="./lambda", platform="linux/amd64"
+            )
+        )
                 
         lambda_function = aws_lambda.Function(self, "function",
             runtime=aws_lambda.Runtime.PYTHON_3_8,
             handler="index.handler",
-            code=code_from_docker
+            code=code
         )
 
 app = App()
